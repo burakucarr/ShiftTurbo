182441b3824d79d0793600adae74dc9b261eb7d8
BURAK UÇAR <burakkucar55@gmail.com>
2026-05-18 18:51:50 +0300

    Bump cache version to v6 to invalidate PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shift-turbo-v5'; // Sürüm yükseltmek zorunlu cache silmeyi tetikler
+const CACHE_NAME = 'shift-turbo-v6'; // Sürüm yükseltmek zorunlu cache silmeyi tetikler
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
